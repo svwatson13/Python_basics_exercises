4d5784023a591d02e00f258eaab63c48868a682c
Sam Watson <SWatson@spartaglobal.com>
2019-12-03 15:53:21 +0000

    First Commit

---
+++ Weather.py
@@ -0,0 +1,11 @@
+user_weather = input('Is it sunny, rainy and or stormy? ')
+if user_weather == 'sunny':
+    print('Shorts all the way my man')
+elif user_weather == 'rainy':
+    print('Take an umbrella')
+elif user_weather == 'stormy':
+    print('Just stay inside')
+elif user_weather == 'rainy and stormy':
+    print('Definitely stay indoors')
+else:
+    print('sorry, i didnt quite catch that')
