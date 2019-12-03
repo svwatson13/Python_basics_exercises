0f612611f72be57e25e9f8f550022d99f9c1bb8d
Sam Watson <SWatson@spartaglobal.com>
2019-12-03 15:54:01 +0000

    First Commit

---
+++ Fizz_Buzz.py
@@ -0,0 +1,31 @@
+# Write a bizz and zzuu game ##project
+
+def fizz_buzz(num):
+    if num%3 == 0 and num%5 == 0:
+        return 'Bizzuu'
+    if num%3 == 0:
+        return 'Bizz'
+    if num%5 == 0:
+        return 'Zzuu'
+    else:
+        return 'Loser'
+print(fizz_buzz(7))
+# write a program that take a number
+# prints back each individual number, but
+    # if it is a multiple of 3 it returns bizz
+    # if a multiple of 5 it return zzuu
+    # if a multiple of 3 and 5 it return bizzzzuu
+
+
+
+
+## separating parts of a computer program into modules that deal with a single feature or behavior
+
+
+# Definition of done for the project:
+# This should be it's own project
+# it should have a read me
+    # it should outline the project
+    # it should have simple instructions on how to run the project
+# it should have git and git history
+# it should be on git hub
